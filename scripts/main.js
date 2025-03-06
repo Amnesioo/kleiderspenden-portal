587f7c0afead5fd49102f8a2f57a72922b32d49f
Simon <simon.weissenberger@iu-study.org>
2025-03-06 17:33:38 +0100

    Schutz vor XSS hinzugefügt

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,17 @@
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
-    alert("Spende wurde erfolgreich abgeschickt!");
+    let name = document.querySelector("#name").value;
+    let escapedName = escapeHTML(name);
+    alert("Spende von " + escapedName + " wurde erfolgreich abgeschickt!");
 });
+
+function escapeHTML(text) {
+    return text.replace(/[&<>"]/g, function(tag) {
+        return {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;'
+        }[tag] || tag;
+    });
+}
